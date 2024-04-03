fb58d36b8a047714b76787f105db40de6aaea52f
vikissj5 <vikissj5@gmail.com>
2024-04-03 19:25:06 +0530

    initial commit

---
+++ src/components/cardList/CardList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './cardList.module.css'
+import Pagination from '../pagination/Pagination'
+function CardList() {
+    return (
+        <div className={styles.container}>CardList
+            <Pagination />
+        </div>
+    )
+}
+
+export default CardList
